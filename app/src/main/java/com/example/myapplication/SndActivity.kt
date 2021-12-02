3b8805bc94f3d017aa36876c3b162494491d3854
akkme <pandoo123@yandex.ru>
2021-12-02 17:39:55 +0500

    3rd task

--- app/src/main/java/com/example/myapplication/SndActivity.kt
+++ app/src/main/java/com/example/myapplication/SndActivity.kt
@@ -1,28 +1,19 @@
 package com.example.myapplication
 
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
+import android.widget.HeaderViewListAdapter
 import android.widget.TextView
 
 private const val HELLO_KEY = "hello!"
 
+class SndActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var nextActivityButton: Button
+    lateinit var helloTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
 
 
     }
-
-
-
 }
